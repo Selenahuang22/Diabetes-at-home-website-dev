2a530aeb4091a9ebc8a78375af9caf7996acccb9
yimchen <yimchen@student.unimelb.edu.au>
2022-05-11 21:12:06 +0800

    add links

--- app.js
+++ app.js
@@ -33,42 +33,51 @@ const patientRouter = require('./routes/patientRouter.js')
 const clinicianRouter = require('./routes/clinicianRouter')
 const healthDataRouter = require('./routes/healthDataRouter')
 
-/* 2 static pages */
+/* static pages */
 app.get('/', async (req, res) => {
-    res.render('aboutWeb.hbs');
+    res.render('S_aboutWeb.hbs');
 })
 app.get('/diabetesInfo', async (req, res) => {
-    res.render('diabetesInfo.hbs');
+    res.render('S_diabetesInfo.hbs');
 })
 
-/*hard code pages now */
+/* hard code pages now */
+/* page for both */
 app.get('/login', async (req, res) => {
-    res.render('login.hbs');
+    res.render('B_login.hbs');
+})
+app.get('/editProfile', async (req, res) => {
+    res.render('B_editProfile.hbs');
+})
+app.get('/viewData', async (req, res) => {
+    res.render('B_viewData.hbs');
 })
 
+/* clician pages */
 app.get('/clinicianSignUp', async (req, res) => {
     res.render('clinicianSignUp.hbs');
 })
-
-app.get('/patientRegister', async (req, res) => {
-    res.render('patientRegister.hbs');
-})
-
 app.get('/clinicianProfile', async (req, res) => {
     res.render('clinicianProfile.hbs');
 })
-
-app.get('/editProfile', async (req, res) => {
-    res.render('editProfile.hbs');
+app.get('/clinicianNotes', async (req, res) => {
+    res.render('clinicianNotes.hbs');
+})
+app.get('/patientRegister', async (req, res) => {
+    res.render('C_patientRegister.hbs');
+})
+app.get('/patientComments', async (req, res) => {
+    res.render('C_patientComments.hbs');
+})
+app.get('/patientProfile', async (req, res) => {
+    res.render('C_patientProfile.hbs');
 })
 
-app.get('/viewData', async (req, res) => {
-    res.render('patientViewData.hbs');
-})
 
-app.get('/cliPatientComments', async (req, res) => {
-    res.render('cliPatientComments.hbs');
-})
+/* patient pages */
+
+
+
 
 /*d2*/
 app.use('/patient/', patientRouter)
@@ -79,7 +88,6 @@ app.use('/health_data/', healthDataRouter)
 app.all('*', (req, res) => {  // 'default' route to catch user errors
 	res.status(404).render('error', {errorCode: '404', message: 'That route is invalid.'})
 })
-
 
 // start server and listen for HTTP requests
 app.listen(port, () => {
